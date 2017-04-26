0f7b0f30ba4198908ccfbd1b1597a6db43e71137
MikroMan <subic.tine@gmail.com>
2017-04-26 13:48:20 +0200

    Added sources and fb chat dump script

--- data/chat/analyze_chat.py
+++ data/chat/analyze_chat.py
@@ -1,39 +1,48 @@
 from bs4 import BeautifulSoup
-from prompt_toolkit.terminal.vt100_input import _Flush
+import lxml
+import pickle
 from time import time
-
+import sys
 
 def parse(path):
     fid = open(path)
     print("Loading html from ", path, '...', end='', flush=True)
     start = time()
-    soup = BeautifulSoup(fid, 'html.parser')
+    soup = BeautifulSoup(fid, 'lxml')
     print("Done: ", time() - start)
 
 #Try bypassing contents directly to thread - eliminate a giant search
 
-    threads = soup.find_all('div', class_='thread')
+    soup = soup.find_all('div', class_='thread')
 
     chats = {}
 
-    print('Found', len(threads), ' threads', flush=True)
-    for thread in threads:
+    print('Found', len(soup), ' threads', flush=True)
+    i = 1
+
+    for thread in soup:
         identifier = thread.contents[0].strip().replace('@facebook.com', '')
         chats[identifier] = {'num_id': identifier, 'msg_count':0, 'msgs': []}
 
-        messages = thread.find_all('div', class_='message')
-        divs = thread.find_all('p')
 
-        for i in range(0, len(messages)):
-            metadata = messages[i]
-            txt = divs[i]
-            msg = {'user': metadata.find_all('span', class_='user')[0].contents, 'txt': txt.text.strip().replace('\n', ' ')}
-            chats[identifier]['msgs'].append(msg)
-            chats[identifier]['msg_count'] += 1
+        children = thread.findChildren()
+
+        msg = {}
+        for child in children:
+            if child.has_attr('class') and child.get('class')[0] == 'user':
+                msg['user'] = child.text
+            elif (not child.has_attr('class')) and child.name == 'p':
+                msg['text'] = child.text
+                chats[identifier]['msgs'].append(msg)
+                chats[identifier]['msg_count'] += 1
+                #print(msg['user'], ':', msg['text'], flush=True)
+                msg = {}
 
 
+        pickle.dump(chats[identifier], open("./dumps/" + str(i)+ ".dump", 'wb' ))
+        i+=1
 
-        print('done')
+    pickle.dump(chats, open('./full_dump.dump', 'wb'))
 
 
 
